0fef9f7d64e48746f7ea24bfc47fb9da23254755
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-17 12:24:50 +0000

    Update Swiper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
-  def create
-    @post = Post.new(post_params)
-      if @post.save
-        redirect_to posts_path
-      else
-        render :new
-      end
+def create
+  @post = Post.new(post_params)
+  if @post.save
+    redirect_to posts_path
+  else
+    @post = Post.new
+    render :new
   end
+end
 
   def index
     @posts = Post.all
